a67f39cde826ebad85c91bff5e9480ebe9d24491
Anand Patil <royalchiku14@gmail.com>
2024-10-26 00:38:51 +0530

    BCN uses .env to get node ip and port

--- WebServer/app.py
+++ WebServer/app.py
@@ -6,11 +6,11 @@ import Orbit_NodeAPI as API
 import os
 import json
 
-#BCN params
-# BCN_ip = '19.ip.gl.ply.gg'
-# BCN_port = 28536
+#BCN_ip = '19.ip.gl.ply.gg'
+#BCN_port = 28536
 
-BCN_ip = os.getenv('BCN_IP')
+#BCN params
+BCN_ip =  os.getenv('BCN_IP')
 BCN_port = int(os.getenv('BCN_PORT'))
 
 API.set_params(BCN_ip, BCN_port)
