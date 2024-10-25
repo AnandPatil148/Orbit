089b25532f8a02cf276d738bce7bd9b3d8aff354
Anand Patil <royalchiku14@gmail.com>
2024-10-26 02:08:42 +0530

    nodal. cpnnectoin and comms fundamentals laids. needs work

--- BlockChainNode/node_handler.py
+++ BlockChainNode/node_handler.py
@@ -8,30 +8,30 @@ def node_handler(node_socket: socket.socket, addr:tuple, isOutgoing:bool, Orbit:
     
     node_chain = []
     
+    if isOutgoing:
+        query = "GET CHAIN" # Gets remote nodes BC info
+        node_socket.send(query.encode(encodeFormat))
+        
+        node_response = node_socket.recv(1024).decode()
+        
+        if node_response.startswith("CHAIN"):
+            node_chain = json.loads(node_response[6:])
+            
+            if  len(node_chain) > len(Orbit.to_dict):
+                Orbit.set_chain(node_chain)
+                print(f"{addr} sent us a longer chain than ours so we have updated our chain")
+            else:
+                # send our chain to node
+                print(f"{addr} sent us a shorter chain but it didn't work")
+
+    
     while True:
         
         try:
             
-            if isOutgoing:
-                query = "GET CHAIN" # Gets remote nodes BC info
-                node_socket.send(query.encode(encodeFormat))
-                
-                node_response = node_socket.recv(4096).decode()
-                
-                if node_response.startswith("CHAIN"):
-                    node_chain = json.loads(node_response[6:])
-                    
-                    if  len(node_chain) > len(Orbit.to_dict):
-                        Orbit.set_chain(node_chain)
-                        print(f"{addr} sent us a longer chain than ours so we have updated our chain")
-                    else:
-                        # send our chain to node
-                        print(f"{addr} sent us a shorter chain but it didn't work")
-            else:
-                pass
+            recvString = node_socket.recv(1024).decode()
             
-
-            recvString = node_socket.recv(4096).decode()
+            print(recvString) # temp
             
             # If server closes connection or client sends QUIT message it will be caught here
             if not recvString:
@@ -40,8 +40,6 @@ def node_handler(node_socket: socket.socket, addr:tuple, isOutgoing:bool, Orbit:
             if recvString == "GET CHAIN":
                 query = "CHAIN " +json.dumps(Orbit.to_dict())
                 node_socket.send(query.encode(encodeFormat))
-
-            
 
         except Exception as e:
             print(f'Error in handling connection from {addr}: {e}') 
